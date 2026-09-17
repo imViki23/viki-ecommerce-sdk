2cad91d57432c09246f874016940074de204a32e
imViki23 <vickyvignesh2308@gmail.com>
2026-09-18 03:37:35 +0530

    Update

--- api/catalog/src/main/resources/db/migration/V1__create_product_and_variants_table.sql
+++ api/catalog/src/main/resources/db/migration/V1__create_product_and_variants_table.sql
@@ -16,6 +16,7 @@ CREATE SCHEMA IF NOT EXISTS catalog;
 -- BRAND
 CREATE TABLE catalog.brands (
     brand_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    title TEXT NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
@@ -27,6 +28,8 @@ CREATE TRIGGER trigger_brand_updated_at
 -- Products
 CREATE TABLE catalog.products (
     product_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    title TEXT NOT NULL,
+    slug VARCHAR(255) NOT NULL UNIQUE,
     brand_id UUID NOT NULL REFERENCES catalog.brands(brand_id),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
@@ -39,24 +42,13 @@ CREATE TRIGGER trigger_products_updated_at
 -- Variants
 CREATE TABLE catalog.variants (
     variant_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    sku VARCHAR(100) NOT NULL UNIQUE,
     product_id UUID NOT NULL REFERENCES catalog.products (product_id),
+    attributes JSONB NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 CREATE TRIGGER trigger_variants_updated_at
     BEFORE UPDATE ON catalog.variants
-    FOR EACH ROW
-    EXECUTE FUNCTION update_updated_at_column();
-
--- Stock
-CREATE TABLE catalog.stocks (
-    variant_id UUID NOT NULL REFERENCES catalog.variants(variant_id),
-    price NUMERIC(12, 2) NOT NULL CHECK ( price >= 0 ),
-    quantity INT NOT NULL CHECK ( quantity >= 0 ),
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
-CREATE TRIGGER trigger_stock_updated_at
-    BEFORE UPDATE ON catalog.stocks
     FOR EACH ROW
     EXECUTE FUNCTION update_updated_at_column();
